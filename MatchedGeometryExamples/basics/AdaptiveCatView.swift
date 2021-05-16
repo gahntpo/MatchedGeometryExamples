e23aab319e8bf6b4c47f9421a825b414d7c1ae64
Karin Prater <karin.prater@gmx.de>
2021-05-16 18:16:49 +0300

    end first tutorial

--- MatchedGeometryExamples/basics/AdaptiveCatView.swift
+++ MatchedGeometryExamples/basics/AdaptiveCatView.swift
@@ -10,8 +10,11 @@ import SwiftUI
 struct AdaptiveCatView: View {
     
     @Environment(\.verticalSizeClass) var veri: UserInterfaceSizeClass?
+
+    @Namespace var namespace
     
     var body: some View {
+        
         
         GeometryReader  { geometry in
             ConditionalStack(vAlignment: .center, hAlignment: .center, vSpacing: 10, hSpacing: 10, isVStack: geometry.size.width < geometry.size.height) {
@@ -24,24 +27,38 @@ struct AdaptiveCatView: View {
                         .scaledToFit()
                         .clipShape(Circle())
                         .frame(width: 100, height: 100, alignment: .center)
+                        
+                    
                     Text("Minime - 2020")
                 }
+                
                 
                 Text("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.")
                     .font(.footnote)
             }
+            .matchedGeometryEffect(id: "text", in: namespace)
             
             Image("minime")
                 .resizable()
                 .scaledToFit()
+                .matchedGeometryEffect(id: "large image", in: namespace)
+                
         }
         .padding()
         }
+        .animation(.easeInOut(duration: 0.5))
+        
     }
 }
 
 struct AdaptiveCatView_Previews: PreviewProvider {
     static var previews: some View {
-        AdaptiveCatView()
+        Group {
+            AdaptiveCatView()
+                .environment(\.verticalSizeClass, .regular)
+            AdaptiveCatView()
+                .environment(\.verticalSizeClass, .compact)
+                .previewLayout(.fixed(width: 800.0, height: 400.0))
+        }
     }
 }
