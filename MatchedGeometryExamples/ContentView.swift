aac36dd37ee4babc6ac0b7e8993b723d600c3e5b
Karin Prater <karin.prater@gmx.de>
2021-05-18 12:37:36 +0300

    added images for readme

--- MatchedGeometryExamples/ContentView.swift
+++ MatchedGeometryExamples/ContentView.swift
@@ -8,9 +8,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-       // AdaptiveCatView()
-//        CatCollectionView()
-        EmojisPicker()
+        TabView {
+            
+            AdaptiveCatView()
+                .tag(0)
+                .tabItem {
+                    Image(systemName: "arrowshape.zigzag.forward")
+                    Text("Swop position") }
+            
+            CatCollectionView()
+                .tag(1)
+                .tabItem {
+                    Image(systemName: "star")
+                    Text("Hero") }
+            
+            PickerExamples()
+                .tag(2)
+                .tabItem {
+                    Image(systemName: "lasso")
+                    Text("Picker") }
+            
+            
+            EmojisPicker()
+                .tag(3)
+                .tabItem {
+                    Image(systemName: "eyedropper")
+                    Text("Hero picker") }
+            
+        }
     }
 }
 
--- MatchedGeometryExamples/Pickers/PickerExamples.swift
+++ MatchedGeometryExamples/Pickers/PickerExamples.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct PickerExamples: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
             Text("Pick something").font(.title)
             
             CirclePicker()
@@ -18,7 +18,7 @@ struct PickerExamples: View {
             Text("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.")
                 .font(.footnote)
             
-            
+            CalenderView()
            
         }.padding()
     }
